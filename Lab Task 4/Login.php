357e1e17464c888dd433ff3eedc2b4fb3621372d
akahammedjamil <101688314+akahammedjamil@users.noreply.github.com>
2022-06-24 23:45:26 +0600

    Add files via upload

---
+++ Lab Task 4/Login.php
@@ -0,0 +1,18 @@
+<style type= "text/css">
+ th {
+  text-align: right;
+ }
+ h3 {
+  text-align: center;
+ }
+</style>
+<table = cellpadding="5" cellspacing="10" align = "center">
+ <h3> Login form using session and cookies with Remember me </h3>
+ <form method="post" action = "validate.php">
+  
+  <tr><th> Username<th><td><input type = "text" name = "username"> </td> </th>
+  <tr><th> Password<th><td><input type = "password" name = "password"> </td> </th>
+   <tr><td colspan = "2" align = "center"> <input type = "checkbox" name = "remember" value = "1"> Remember Me</td></tr>
+   <tr><td colspan = "2" align = "right"><input type = "submit" value = "Login" name= "login"></td></tr>
+   </form>
+   </table> 
